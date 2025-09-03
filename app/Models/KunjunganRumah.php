fbd9598d86d41f2cdc6ce29a53dfad657d8682c7
adisyaerma <adisyaermawijayanti@gmail.com>
2025-09-03 11:39:46 +0700

    model configuration

--- app/Models/KunjunganRumah.php
+++ app/Models/KunjunganRumah.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class KunjunganRumah extends Model
 {
     //
+    use HasFactory, Notifiable;
+
+    protected $table = 'kunjungan_rumahs';
+    protected $id = ['id'];
 }
